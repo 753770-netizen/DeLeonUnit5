8b3bd3c96b128f4d8f3c7198a2d4e57823b22ffd
753770-netizen <753770@student.dusd.net>
2026-05-22 13:46:13 -0700

    Finished everything

---
+++ P6DeLeonUnit5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 7); // destroy particle after 7 seconds
+    }
+
+
+}
